c86bbad2de2bd2cf4f100920453e340f5a6f9e5b
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-04-12 00:58:22 +0600

    array function

--- Basic-ES6/secript/arrayFunction.js
+++ Basic-ES6/secript/arrayFunction.js
@@ -1,5 +1,45 @@
-function sum (num1, num2){
-return num1 + num2;
+// function sum (num1, num2){
+// return num1 + num2;
+// }
+// const total =  sum(10, 10);
+// console.log(total);
+
+// array function
+// const sum = (num1, num2) =>{
+// return num1 + num2
+// }
+// console.log(sum(20, 30));
+//  const diff = (num1, num2) => num1 - num2;
+//  console.log(diff(20, 10));
+ 
+//  const add = (num) =>{
+//     console.log("number =",num); 
+//  }
+//  add(77)
+
+document.getElementById("title").addEventListener("click", 
+   () =>{
+ console.log("division", 11);
+ 
+    }
+ )
+
+function sum (num, num1,num2 ){
+    const total = num + num1 + num2;
+return total;
 }
-const total =  sum(10, 10);
-console.log(total);
+const total = sum(10, 20, 30);
+console.log("3 number total =",total);
+
+ const add = (num, num1, num2) =>{
+return num + num1 + num2;
+ }
+ console.log(add("3 number add =",20, 30, 20));
+ 
+ const multiply = (num1, num2) =>{
+console.log("multiply =",num1 * num2);
+ }
+ multiply(10, 5);
+
+ const diff = (num1, num2) => num1 - num2;
+console.log(diff( 100, 90));
