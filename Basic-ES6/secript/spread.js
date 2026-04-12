8f93d076feb6105c66a085c032ccd16f3c8e08b6
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-04-12 11:32:34 +0600

    added spread oparator and math. max/min

---
+++ Basic-ES6/secript/spread.js
@@ -0,0 +1,9 @@
+const friends = ["sojol", "fozol", "kazol"];
+// console.log(...friends);
+const friend = ["suchorita", ...friends];
+console.log(friend);
+
+const numbers = [1,45, 345, 56, 78, 3,4, 987, 45, 34, 32, 44];
+console.log(Math.max(...numbers));
+console.log(Math.min(...numbers));
+
